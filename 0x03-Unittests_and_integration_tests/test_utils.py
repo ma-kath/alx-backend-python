25235bbbfdaf539c3bf2283926c4c9c314db468b
ma-kath <makathosn@gmail.com>
2025-08-31 18:52:45 +0200

    Mock http calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,8 +2,10 @@
 """Unit tests for utils.access_nested_map function."""
 
 import unittest
+from unittest.mock import Mock, patch
+ 
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 class TestAccessNestedMap(unittest.TestCase):
     """Test case for utils.access_nested_map function."""
@@ -26,3 +28,21 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as cm:
             access_nested_map(nested_map, path)
         self.assertEqual(cm.exception.args[0], path[-1])
+        
+
+class TestGetJson(unittest.TestCase):
+    """Test case for utils.get_json function."""
+    
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @patch("utils.requests.get")
+    def test_get_json(self, test_url: str, test_playload: dict, mock_get: Mock):
+        """Test that get_json returns expected payload and calls requests.get once."""
+        mock_get.return_value.json.return_value = test_playload
+        
+        result = get_json(test_url)
+        
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(result, test_playload)
