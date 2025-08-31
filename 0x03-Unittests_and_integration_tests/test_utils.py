aaeaecd76a19a12262ad6004491f4d8caac2c06d
ma-kath <makathosn@gmail.com>
2025-08-31 19:01:26 +0200

    Param and patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,7 +5,7 @@ import unittest
 from unittest.mock import Mock, patch
  
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 class TestAccessNestedMap(unittest.TestCase):
     """Test case for utils.access_nested_map function."""
@@ -46,3 +46,31 @@ class TestGetJson(unittest.TestCase):
         
         mock_get.assert_called_once_with(test_url)
         self.assertEqual(result, test_playload)
+    
+class TestMemoize(unittest.TestCase):
+    """Test case for the memoize decorator."""
+    
+    def test_memoize(self):
+        """Test that memoize caches the result of a method call."""
+        
+        class TestClass:
+            """Test class with a memoized property."""
+        
+            def a_method(self) -> int:
+                """Test class with a memoized property."""
+                return 42
+            
+            @memoize
+            def a_property(self):
+                """Memoized property that calls a_method."""
+                return self.a_method()
+        
+        test_obj = TestClass()
+        
+        with patch.object(test_obj, 'a_method', wraps=test_obj.a_method) as mock_method:
+            result1 = test_obj.a_property
+            result2 = test_obj.a_property
+            
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+            mock_method.assert_called_once()
