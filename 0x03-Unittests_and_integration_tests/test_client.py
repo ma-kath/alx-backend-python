ac6373cba45d8e985ec1b6c21677ff096ee6e625
ma-kath <makathosn@gmail.com>
2025-09-01 13:15:50 +0200

    Checking

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -37,3 +37,27 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_org.return_value = test_playload
             result = client._public_repos_url
             self.assertEqual(result, test_playload["repos_url"])
+            
+    @patch("client.get_json")
+    def test_public_repos(self, mock_get_json):
+        """Test that mocked props and return once."""
+        mocked_repos_payload = [
+            {"name": "repo1"},
+            {"name": "repo2"},
+            {"name": "repo3"},
+        ]
+        mock_get_json.return_value = mocked_repos_payload
+        client = GithubOrgClient("testorg")
+        with patch.object(
+            type(client),
+            "public_repos_url",
+            new_callable=property
+        ) as mock_public_repos_url:
+            test_url = "https://api.github.com/orgs/testorg/repos"
+            mock_public_repos_url.fget = lambda self: test_url
+            repos = client.public_repos()
+            repos_names = [repo for repo in repos]
+            expected_names = [repo["name"] for repo in mocked_repos_payload]
+            self.assertEqual(repos_names, expected_names)
+            self.assertEqual(mock_public_repos_url.fget.call_count, 1)
+            mock_get_json.assert_called_once_with(test_url)
