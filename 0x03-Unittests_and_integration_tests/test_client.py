6859b7b601bb700bd06b72d4228cd811c923ac89
ma-kath <makathosn@gmail.com>
2025-09-01 12:32:14 +0200

    Renaming good files

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Unit tests for client.GithubOrgClient class."""
+
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Test case for GithubOrgClient class."""
+    @parameterized.expand([
+        ("google",),
+        ("abc",),
+    ])
+    @patch("client.get_json")
+    def test_org(self, org_name: str, mock_get_json):
+        """Test that GOC return expected value"""
+        mock_get_json.return_value = {"mocked": "data"}
+        client = GithubOrgClient(org_name)
+        result = client.org
+        mock_get_json.assert_called_once_with(
+            f"https://api.github.com/orgs/{org_name}")
+        self.assertEqual(result, mock_get_json.return_value)
